61ce428dfcb865191b0a780426156b8032a5f807
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-26 13:46:30 +0100

    create de class contact

---
+++ FrontEndfile/src/app/Modeles/Contact.ts
@@ -0,0 +1,5 @@
+export interface Contact{
+  id_contact: number; // Assuming this is auto-generated, you might not need to set it on the front end
+  Email: string;      // Note: 'Email' is capitalized
+  phone: string;
+  message: string;}
