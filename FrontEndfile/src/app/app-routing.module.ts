c8e83f294b50d482ff52cc0c70ec8d9b590faf98
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-29 21:35:17 +0100

    modifier dans les routes

--- FrontEndfile/src/app/app-routing.module.ts
+++ FrontEndfile/src/app/app-routing.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {BarreNavigationComponent} from "./barre-navigation/barre-navigation.component";
-import {SalleComponent} from "./salle/salle.component";
-import {RegisterComponent} from "./register/register.component";
-import {HomePageComponent} from "./home-page/home-page.component";
-import {AdminPageComponent} from "./admin-page/admin-page.component";
-import {AcceuilComponent} from "./acceuil/acceuil.component";
-import {ListSalleComponent} from "./list-salle/list-salle.component";
+import {BarreNavigationComponent} from "./Page/barre-navigation/barre-navigation.component";
+import {SalleComponent} from "./Page/salle/salle.component";
+import {RegisterComponent} from "./Page/register/register.component";
+import {HomePageComponent} from "./Page/home-page/home-page.component";
+import {AdminPageComponent} from "./Page/admin-page/admin-page.component";
+import {AcceuilComponent} from "./Page/acceuil/acceuil.component";
+import {ListSalleComponent} from "./Page/list-salle/list-salle.component";
 import {LoginComponent} from "./Page/login/login.component";
-import {FooterComponent} from "./footer/footer.component";
-import {DeclareSalleComponent} from "./declare-salle/declare-salle.component";
-import {AboutComponent} from "./about/about.component";
+import {FooterComponent} from "./Page/footer/footer.component";
+import {DeclareSalleComponent} from "./Page/declare-salle/declare-salle.component";
+import {AboutComponent} from "./Page/about/about.component";
+import {ReservationComponent} from "./Page/reservation/reservation.component";
 
 const routes: Routes = [
-  {path:"",redirectTo:"acceuil",pathMatch:'full'},
-  { path: "navbar", component: BarreNavigationComponent },
-  { path: "salle", component: SalleComponent },
-  { path: "register", component: RegisterComponent },
-  {path:"découvre",component:FooterComponent},
-  { path: "footer", component: HomePageComponent },
+  {path:"",redirectTo:'acceuil',pathMatch:'full'},
+  { path: 'navbar', component: BarreNavigationComponent },
+  { path: 'salle-details/:id', component: SalleComponent },
+  {path:'reservation/:id',component:ReservationComponent},
+  { path: 'register', component: RegisterComponent },
+  {path:'découvre',component:FooterComponent},
+  { path: 'footer', component: HomePageComponent },
   { path: "login", component: LoginComponent },
   { path: "list", component: ListSalleComponent },
   { path: "statistiques", component: AdminPageComponent },
