de5968ecf7258b7144ca0cd295fa0d3a909dd9e3
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-29 21:29:34 +0100

    creation de logique pour reserver

--- FrontEndfile/src/app/Page/service/reservationservice/reservation.service.ts
+++ FrontEndfile/src/app/Page/service/reservationservice/reservation.service.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {Reservation} from "./Modeles/Reservation";
+import {Reservation} from "../../../Modeles/Reservation";
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
-  private apiUrl = 'http://localhost:9090/api/reservation/faire'; // URL de votre API
+  private baseUrl = 'http://localhost:9090/api/reservation'; //
 
   constructor(private http: HttpClient) { }
 
-  createReservation(reservation:Reservation): Observable<Reservation> {
-    return this.http.post<Reservation>(this.apiUrl, reservation);
+  // Méthode pour créer une réservation en associant l'ID de la salle
+  createReservation(idSalle: number, reservation: Reservation): Observable<any> {
+    return this.http.post(`${this.baseUrl}/faire/${idSalle}`, reservation);  // Corrige l'URL et la variable
   }
 
-  getAll(){
-
-  }
 }
